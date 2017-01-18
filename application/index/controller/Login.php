987b466bd6ec117d07de822d181d7941524fbb9c
LSH <2500931932@qq.com>
2017-01-18 21:59:04 +0800

    新建登录

---
+++ application/index/controller/Login.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LuSonghua
+ * Date: 2017/1/18
+ * Time: 21:53
+ */
+
+namespace app\index\controller;
+
+
+class Login extends Index
+{
+    public function index(){
+        return $this->fetch();
+    }
+
+}
