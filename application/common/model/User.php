022efa3d5c81972748e379132a5715c7d53b34ff
LSH <2500931932@qq.com>
2017-01-19 21:54:26 +0800

    编写登录

---
+++ application/common/model/User.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: LuSonghua
+ * Date: 2017/1/19
+ * Time: 20:46
+ */
+class User extends \think\Model
+{
+    /*
+     * id 主键 name,username,phone,email,type 用户类型
+     * type: 用户类型
+     * status: 默认1，1正常，0不正常
+     */
+}
